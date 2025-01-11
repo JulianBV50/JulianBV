d074086b76334dd7f02fe3c70c5aaf7be3a31fc4
Julian Bedoya Vargas <julianbv@outlook.com>
2025-01-11 15:08:30 -0500

    Manejo de tablas y chart

--- app.py
+++ app.py
@@ -4,8 +4,31 @@ import pandas as pd
 
 st.title("Elementos de interfase :iphone:")
 
+###
+# static table
+###
 st.header("tabla statica")
 dataframe = pd.DataFrame(
     np.random.randn(10,20),
     columns=("col %d" % i for i in range(20)))
 st.table(dataframe)
+
+###
+# Dynamic DF
+###
+st.header("Dynam,ic DataFrame")
+dataframe=pd.DataFrame(
+    np.random.rand(10,20),
+    columns=("col %d" % i for i in range(20)))
+
+st.dataframe(dataframe.style.highlight_max(axis=0))
+
+###
+# Chart
+###
+st.header("Line Chart")
+chart_data=pd.DataFrame(
+    np.random.randn(20,3),
+    columns=['a','b','c'])
+
+st.line_chart(chart_data)
